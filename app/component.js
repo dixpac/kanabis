989625a21a39361ff4f815cbad9234e0a87b2b90
dixpac <dino.onex@gmail.com>
2016-01-30 10:08:13 +0100

    basic dev setup

---
+++ app/component.js
@@ -0,0 +1,9 @@
+module.exports = function() {
+  var element = document.createElement('h1');
+
+  element.innerHTML = 'Hello';
+
+  return element;
+};
+
+
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: PATHS.app,
+  output: {
+    app: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }  
+    ]
+  }
+};
+
+// Default configuraiton
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
